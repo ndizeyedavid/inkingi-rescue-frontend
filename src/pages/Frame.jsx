ef56c97248a090efaf1c87ef53d5355dbc1ddaa6
ndizeyedavid <davidndizeye101@gmail.com>
2025-09-10 12:29:09 +0200

    Updated Iframce source

--- src/pages/Frame.jsx
+++ src/pages/Frame.jsx
@@ -6,7 +6,7 @@ function Frame() {
                <div className="mockup-phone scale-[77%]">
                     {/* <div className="mockup-phone-camera"></div> */}
                     <div className="mockup-phone-display">
-                         <iframe src="http://localhost:5173" className='h-full w-[101%]'></iframe>
+                         <iframe src="https://inkingirescue.netlify.app" className='h-full w-[101%]'></iframe>
                     </div>
                </div>
           </section>
